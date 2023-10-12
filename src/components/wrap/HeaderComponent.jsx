4e78ba4b9cd4d061a226cee95345df3743fe1676
KIMWOOSIK <135534596+kimwoosik123@users.noreply.github.com>
2023-10-12 15:55:31 +0900

    docs: Update SignupComponent
    회원가입창 디자인 변경 및 모달 기능, 라벨링 기능 추가

--- src/components/wrap/HeaderComponent.jsx
+++ src/components/wrap/HeaderComponent.jsx
@@ -1,38 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom';
-import SignModalComponent from './user/SignModalComponent';
 
 export default function HeaderComponent() {
-  const [isModal, setIsModal] = React.useState(false);
-  
-  const onClickSignup = (e) => {
-    if(isModal === false){
-      setIsModal(true);
-    }
-  };
   return (
     <>
       <header id="header">
-        <div className="container">
-          <div className="gap">
-            <div className="content">
-              <div className="content__left">
-                <hr />
-                <hr />
-                <hr />
-              </div>
-              <div className="content__right">
-                <div className="btn__box">
-                  <button className="signin__btn" type='button'>로그인</button>
-                  <button className="signup__btn" type='button' onClick={onClickSignup}>회원가입</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        {
-          isModal ? <SignModalComponent setIsModal={setIsModal} /> : null
-        }
+        <h1>header</h1>
       </header>
       <Outlet />
     </>
